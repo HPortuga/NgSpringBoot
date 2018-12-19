c0d5d471540f501dd702f7f264b9c7b7658403d0
Matheus Henrique <mathsoares1@gmail.com>
2018-12-19 16:17:32 -0300

    passando parametros na rota; criação do componente listTodos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
-  {path: '**', component: ErrorComponent},
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'welcome', component: WelcomeComponent}
+  {path: 'welcome/:name', component: WelcomeComponent},
+  {path: '**', component: ErrorComponent}
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,24 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  
   username = "matheus";
   password = '';
   errorMessage = 'Invalid Credentials';
   invalidLogin = false;
-
-  constructor() { }
-
+  
+  constructor(private router: Router) { }
+  
   ngOnInit() {
   }
-
+  
   handleLogin() {
     if (this.username === "matheus" && this.password === "pass") {
+      this.router.navigate(['welcome', this.username]);
       this.invalidLogin = false;
     }
     else {
       this.invalidLogin = true;
     }
   }
-
+  
 }
