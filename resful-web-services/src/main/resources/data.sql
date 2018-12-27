56df7d1d21759b937cb4474cb5d5c3420c31bcc4
Matheus Henrique <mathsoares1@gmail.com>
2018-12-27 11:36:06 -0300

    Configurado h2

---
+++ resful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values (1001, 'matheus', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (1002, 'matheus', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (1003, 'matheus', 'Learn Microservices', sysdate(), false);
